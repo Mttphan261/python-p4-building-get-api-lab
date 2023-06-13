aeb3eae5b7e475f7c37137b32c4370894d671bf6
Matthew Phan <mttphan261@gmail.com>
2023-06-13 07:25:01 -0500

    lab complete but pytest failing

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, make_response, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
@@ -18,21 +19,63 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
+# GET /bakeries: returns an array of JSON objects for all bakeries in the database.
 @app.route('/bakeries')
 def bakeries():
-    return ''
+
+    all_bakeries = []
+    bakeries = Bakery.query.all()
+    for each in bakeries:
+        bakery = each.to_dict()
+        all_bakeries.append(bakery)
+
+    response = make_response(
+        jsonify(all_bakeries),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response 
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    # bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery = Bakery.query.filter_by(id=id).first()
 
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        jsonify(bakery_dict),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+# GET /baked_goods/by_price: returns an array of baked goods as JSON, sorted by price in descending order. (HINT: how can you use SQLAlchemy to sort the baked goods in a particular order?)
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    all_baked_goods = []
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    for each in baked_goods:
+        each_dict = each.to_dict()
+        all_baked_goods.append(each_dict)
+    response = make_response(
+        jsonify(all_baked_goods),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).all()
+    for each in most_expensive:
+        most_expensive_dict = each.to_dict()
+    response = make_response(
+        jsonify(most_expensive_dict),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 if __name__ == '__main__':
     app.run(port=555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,10 +11,32 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    serialize_rules = ('-baked_goods.bakery',)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    updated_at = db.Column(db.DateTime, nullable=True)
+
+    baked_goods = db.relationship('BakedGood', backref ='bakery')
+
+    def __repr__(self):
+        return f'<Bakery {self.name}>'
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakery.baked_goods',)
+
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime)
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    updated_at = db.Column(db.DateTime, nullable=True)
+
+    def __repr__(self):
+        return f'<Baked Good {self.name}, ${self.price}>'
+
+
     
